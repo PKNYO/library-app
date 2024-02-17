83e615c969c5c1cde4ca5e4cd67ac91e573cd892
PKNYO <147757429+PKNYO@users.noreply.github.com>
2024-02-17 19:05:56 +0100

    Added append book functionality
    I added the functionality in the js code to
    add a new card in the grid with the entered
    informations.

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
-const addButton = document.querySelector(".add-button")
+const addButton = document.querySelector(".add-button");
+const libraryContainer = document.querySelector(".lybrary-container");
+const createCard = document.querySelector(".create-card");
 
 let myLibrary = [];
 
@@ -14,6 +16,35 @@ function addBookToLibrary(book) {
     console.log(myLibrary)
 }
 
+function createBookCard(name, author, pages, read) {
+    const cardDiv = document.createElement("div");
+    const h2 = document.createElement("h2");
+    const h3 = document.createElement("h3");
+    const p = document.createElement("p");
+    const readDiv = document.createElement("div");
+    const label = document.createElement("label");
+    const input = document.createElement("input");
+
+    cardDiv.classList.add("card");
+
+    input.setAttribute("type", "checkbox")
+    input.setAttribute("id", "read")
+
+    createCard.parentNode.insertBefore(cardDiv, createCard.nextSibling);
+    cardDiv.appendChild(h2);
+    cardDiv.appendChild(h3);
+    cardDiv.appendChild(p);
+    cardDiv.appendChild(readDiv);
+    readDiv.appendChild(label);
+    readDiv.appendChild(input);
+
+    h2.textContent = name;
+    h3.textContent = author;
+    p.textContent = pages;
+    label.textContent = "Read:"
+    input.checked = read;
+}
+
 addButton.addEventListener(("click"), () => {
     let name = document.querySelector("#name");
     let author = document.querySelector("#author");
@@ -23,6 +54,7 @@ addButton.addEventListener(("click"), () => {
     const newBook = new Book(name.value, author.value, pages.value, read.checked);
 
     addBookToLibrary(newBook);
+    createBookCard(name.value, author.value, pages.value, read.checked);
 
     name.value = "";
     author.value = "";
