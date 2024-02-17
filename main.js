0fd39329227741e3937924d95e1701ca475efebf
PKNYO <147757429+PKNYO@users.noreply.github.com>
2024-02-17 18:29:17 +0100

    The form card can now add a book in array
    We can add a book in the array and the card reset
    itself.
    Need to show it at screen now.

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+const addButton = document.querySelector(".add-button")
+
 let myLibrary = [];
 
 function Book(name, author, pages, read) {
@@ -9,4 +11,21 @@ function Book(name, author, pages, read) {
 
 function addBookToLibrary(book) {
     myLibrary.unshift(book);
+    console.log(myLibrary)
 }
+
+addButton.addEventListener(("click"), () => {
+    let name = document.querySelector("#name");
+    let author = document.querySelector("#author");
+    let pages = document.querySelector("#pages");
+    let read = document.querySelector("#read");
+
+    const newBook = new Book(name.value, author.value, pages.value, read.checked);
+
+    addBookToLibrary(newBook);
+
+    name.value = "";
+    author.value = "";
+    pages.value = "";
+    read.checked = false;
+})
